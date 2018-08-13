10f7979349382db174c16f60278f04f097cb7244
Arpanjparasar <arpangeet@gmail.com>
2018-08-13 10:59:53 +0530

    learning Inheritance and abstraction

--- practise5.py
+++ practise5.py
@@ -1,17 +1,30 @@
+import random
 class passenger:
     def __init__(self,name,contact):
         self.name=name
         self.contact=contact
+        self.pnr=random.randint(5000,20000)
+class first(passenger):
+    def __init__(self,name,contact):
+        super().__init__(name,contact)
 
-    def first(self):
+    def display(self):
         print('Name is :', self.name)
         print('Contact is :', self.contact)
+        print('PNR number is :', self.pnr)
+        print('Meals are free')
 
+class second(passenger):
+    def __init__(self,name,contact):
+        super().__init__(name,contact)
 
-    def second(passenger):
-        pass
+    def display(self):
+        print('Name is :', self.name)
+        print('Contact is :', self.contact)
+        print('PNR number is :', self.pnr)
+        print('Meals are not free')
 
 name=input('Enter the name')
 contact=input('Enter the contact no')
 obj = passenger(name,contact)
-obj.first()
+obj.display()
