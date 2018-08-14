ac6e53d42d2a12fefcfc8123a013471f53b096c5
Arpanjparasar <arpangeet@gmail.com>
2018-08-15 00:41:22 +0530

    implementing threads

--- practise6.py
+++ practise6.py
@@ -5,20 +5,42 @@ import random
 
 class details:
     def inputs(self):
-        a1 = input('Enter first name :')
-        a2 = input('Enter second name :')
-        a3 = input('Enter third name :')
-        a4 = input('Enter fourth name :')
-        a5 = input('Enter fifth name :')
-        foo=[a1,a2,a3,a4,a5]
-        print(foo)
+        name=[]
+        for i in range(5):
+            time.sleep(0.2)
+            a=input('Enter the names :')
+            name.append(a)
 
-        foo1={}
-        for i in foo:
-            r = str(random.randint(50, 300))
-            foo1[i]=i+r+'@mail.com'
+        print(name)
+        return name
 
-        print(foo1)
+    def email(self,b):
+        email=[]
+        for i in range(len(b)):
+            time.sleep(0.2)
+            email.append(b[i]+ str(random.randint(50, 300))+'@mail.com')
+
+
+        print(email)
+        return email
+
+    def enter(self,email,name):
+        data=open(r'E:\Sgraph Python practice\detailspractise6.txt','a')
+        k = str(name)
+        s = str(email)
+        data.write(k)
+        data.write(s)
+
+
 
 obj= details()
-obj.inputs()
+t=time.time()
+b=obj.inputs()
+c=obj.email(b)
+t1=threading.Thread(target=obj.inputs, args=(b,))
+t2=threading.Thread(target=obj.email, args=(c,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print('Done in ', time.time()-t)
