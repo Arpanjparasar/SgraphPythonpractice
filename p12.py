78cd0dee04fbe0d1de84e072dec0ff998657c56a
Arpanjparasar <arpangeet@gmail.com>
2018-09-05 10:19:27 +0530

    practice logics

---
+++ p12.py
@@ -0,0 +1,14 @@
+class emp:
+    def __init__(self,name,age,location):
+        self.name=name
+        self.age=age
+        self.location=location
+    def display(self):
+        print('Name is :',self.name)
+        print('Age is :', self.age)
+        print('Location is :', self.location)
+
+class developer:
+
+    def display(self):
+        pass
